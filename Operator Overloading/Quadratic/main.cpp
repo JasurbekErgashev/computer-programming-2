f1239494dd8c9065a6f5d87d3a85cc4e0f240549
Jasurbek Ergashev <84211518+JasurbekErgashev@users.noreply.github.com>
2022-06-02 18:19:11 +0500

    Update main.cpp

--- Operator Overloading/Quadratic/main.cpp
+++ Operator Overloading/Quadratic/main.cpp
@@ -3,11 +3,24 @@
 int main() {
 
 	Quadratic q1{ 1, 3, 2 };
+	Quadratic q2{ 1, 2, 1 };
 
-	cout << q1 << "\n\n";
-
+	cout << q1 << "\n";
 	q1.solve(1, 3, 2);
+	cout << "\n\n";
 
+	cout << q2 << "\n";
+	q2.solve(1, 2, 1);
+	cout << "\n\n";
+
+	cout << "Addition: \n";
+	Quadratic q3 = q1 + q2;
+	cout << q3;
+	cout << "\n\n";
+
+	cout << "Substraction: \n";
+	Quadratic q4 = q1 - q2;
+	cout << q4;
 	cout << "\n\n";
 
 	system("pause>0");
